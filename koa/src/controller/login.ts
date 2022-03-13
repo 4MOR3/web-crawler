fe3e16ba175696f2f940d0351bf73efd4c64b07f
yanghaoxiang <yanghaoxiang@ehualu.com>
2022-03-13 20:46:36 +0800

    login comp

--- koa/src/controller/login.ts
+++ koa/src/controller/login.ts
@@ -2,7 +2,12 @@ import Koa from 'koa'
 
 async function login(ctx: Koa.Context, next: Koa.Next) {
   const key = ctx.query.password;
+  key === "990121"
+    ? ctx.response.body = { flag: 1 }
+    : ctx.body = {flag: 0};
   console.log(key)
+  console.log(ctx.body)
+  
   await next();
 }
 export default login
--- react-web/src/Login/Login.tsx
+++ react-web/src/Login/Login.tsx
@@ -1,9 +1,17 @@
 import './Login.scss'
 import { Form, Input, Button } from 'antd';
 import { LockOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom'
 
 function Login() { 
-  function onFinish() { }
+  const nav = useNavigate();
+  async function onFinish(e: any) { 
+    const resp = await fetch(`/login?password=${e.password}`)
+    const data = await resp.json();
+    if (data.flag) { 
+      nav(`/staticCrawler`)
+    }
+  }
   function onFinishFailed() { }
   return (
     <div className='login-form'>
